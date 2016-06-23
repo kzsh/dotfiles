7ab479dfb4b04575ca9c16da5c1931e4dab332bd
Andrew Hunt <andrew@hunt.li>
2016-06-23 15:58:59 -0400

    Add a few casks for other apps I use

--- install/install_casks.sh
+++ install/install_casks.sh
@@ -1,4 +1,7 @@
+brew cask install quicksilver
 brew cask install google-chrome
+brew cask install google-chrome-canary
 brew cask install slack
 brew cask install iterm2-beta
 brew cask install sequel-pro
+brew cask install limechat
