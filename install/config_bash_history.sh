1bebf5efb20f3581688be8c64df637005514fe92
Andrew Hunt <andrew@hunt.li>
2016-06-23 15:58:22 -0400

    Spacing

--- install/config_bash_history.sh
+++ install/config_bash_history.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 export HISTCONTROL=ignoreboth:erasedups
-
 export HISTSIZE=100000                   # big big history
 export HISTFILESIZE=100000               # big big history
-shopt -s histappend                      # append to history, don't overwrite it
 
+shopt -s histappend                      # append to history, don't overwrite it
 shopt -s histverify
 
 export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi'
