adc2fc1c5328d9b9c8b2cec43c95ec9ee586c52c
Andrew Hunt <andrew@hunt.li>
2016-08-06 15:02:59 -0400

    Create requisite dir if !exists

--- install/config_bash_history.sh
+++ install/config_bash_history.sh
@@ -6,6 +6,8 @@ export HISTFILESIZE=100000               # big big history
 shopt -s histappend                      # append to history, don't overwrite it
 shopt -s histverify
 
+mkdir -p "$HOME/.logs"
+
 export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi'
 
 # Save and reload the history after each command finishes
