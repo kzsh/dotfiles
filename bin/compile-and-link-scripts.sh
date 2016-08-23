028dec6d86ec1bdd9875ec8655a427935739b4b2
Andrew Hunt <andrew@hunt.li>
2016-08-22 20:56:52 -0400

    Create top level compile-and-link-to-workflow script

---
+++ bin/compile-and-link-scripts.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+CWD="$(pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$HOME/bin/util.sh"
+
+goToPath "$DIR/.."
+BASE_DIR="$PWD"
+SCRIPT_DIST_DIR="$BASE_DIR/applescripts/dist"
+
+
+function main() {
+  beginSection "Compile scripts"
+    goToPath "$BASE_DIR/applescripts"
+    ./compileScripts.sh
+  endSection
+
+
+  beginSection "Link scripts"
+    goToPath "$BASE_DIR/automator"
+
+    script_file_names=$(ls "$SCRIPT_DIST_DIR/"*.service.js.scpt)
+    for file_name in $script_file_names; do
+      buildWorkflowWrapper "$file_name"
+    done
+  endSection
+
+  goToPath "$CWD"
+}
+
+function buildWorkflowWrapper() {
+  local script_config
+  local target_application
+  local file_name="$1"
+
+  script_config=$(head -n1 "$file_name" | awk '{ print $2 " " $3}')
+  target_application=$(echo "$script_config" | cut -d'=' -f2 | tr -d '"' )
+  ./makeWorkflow.sh "$file_name" "$target_application"
+}
+
+main "$@"
+
