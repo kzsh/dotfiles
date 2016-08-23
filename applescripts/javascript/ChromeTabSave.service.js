b50c03f81cc1527670c018207aa9ee36c552fe54
Andrew Hunt <andrew@hunt.li>
2016-08-22 20:54:01 -0400

    Rename save tab applescript

---
+++ applescripts/javascript/ChromeTabSave.service.js
@@ -0,0 +1,27 @@
+// target="/Applications/Google Chrome.app"
+var currentApp = Application.currentApplication();
+
+currentApp.includeStandardAdditions = true;
+
+try {
+  if (currentApp.title() === 'Google Chrome' ||
+      currentApp.title() === 'Google Chrome Canary') {
+    var chrome = currentApp;
+
+    var tab = chrome.windows.at(chrome.activeWindow).activeTab
+    try {
+      result = app.doShellScript('grep "' + tab.url() + '" ~/TODO/Reading.md')
+      notify("Already stored.");
+    } catch(e) {
+      currentApp.doShellScript('echo "- [ ' + tab.title() + ' ](' + tab.url() + ')" >> ~/TODO/Reading.md')
+      notify(tab.title() + " saved.");
+    }
+  }
+} catch(e) {
+  notify("Couldn't find a tab.  Are you in Chrome or Chrome Canary?")
+}
+
+function notify(notification) {
+  // use currentApp to avoid a privilege violation
+  currentApp.displayNotification(notification)
+}
