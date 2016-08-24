b4d896fe8aeca228728610c0b3f6cce6fc03ef12
Andrew Hunt <andrew@hunt.li>
2016-08-24 13:49:09 -0400

    versions is replaced by boneyard

--- install/install_homebrew.sh
+++ install/install_homebrew.sh
@@ -1,5 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Allow fixing to specific versions
-brew tap homebrew/versions
+brew tap homebrew/boneyard
 brew tap caskroom/versions
