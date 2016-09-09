cc55a53ab59273a8a0a7f9957bd2fb60aaf26b2a
Andrew Hunt <andrew@hunt.li>
2016-09-09 10:12:11 -0400

    Fix lint issues

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/install/install_homebrew.sh
-$DIR/install/install_brews.sh
-$DIR/install/install_casks.sh
-$DIR/install/setup_dotfiles.sh
-$DIR/install/osx_preferences.sh
+"$DIR/install/install_homebrew.sh"
+"$DIR/install/install_brews.sh"
+"$DIR/install/install_casks.sh"
+"$DIR/install/setup_dotfiles.sh"
+"$DIR/install/osx_preferences.sh"
