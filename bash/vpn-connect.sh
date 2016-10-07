4036aee19350e207861a003f44c74e77a4aea6e4
Andrew Hunt <andrew@hunt.li>
2016-10-07 17:44:38 -0400

    Add vpn-connecting script

---
+++ bash/vpn-connect.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+KEYSTORE_PASSWORD_LABEL="$1"
+VPN_NAME="$2"
+USER_NAME="$3"
+function vpn-toggle-connect {
+/usr/bin/env osascript <<-EOF
+set vpn_name to "'$VPN_NAME'"
+set user_name to "$USER_NAME"
+set keystore_password_label to "$KEYSTORE_PASSWORD_LABEL"
+
+tell application "System Events"
+  set rc to do shell script "scutil --nc status " & vpn_name
+  if rc starts with "Connected" then
+    do shell script "scutil --nc stop " & vpn_name
+
+    display notification with title "Disconnected from " & vpn_name
+  else
+    set PWScript to "security find-generic-password -l \"$KEYSTORE_PASSWORD_LABEL\" -w"
+    set passwd to do shell script PWScript
+
+    do shell script "scutil --nc start " & vpn_name & " --user " & user_name
+
+    delay 2
+    keystroke passwd
+    keystroke return
+
+    display notification with title "Connected to " & vpn_name
+  end if
+end tell
+EOF
+}
+
+vpn-toggle-connect
