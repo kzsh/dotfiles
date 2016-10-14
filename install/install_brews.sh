80fd012a0e241d83a9825d7ca4628eb57313bd1c
Andrew Hunt <andrew@hunt.li>
2016-10-13 20:24:41 -0400

    Update post-install setup, add a few brew packages

--- install/install_brews.sh
+++ install/install_brews.sh
@@ -1,20 +1,21 @@
-brew install bash
 brew install ag
-brew install wget
-brew install python3
-brew install tmux
-brew install tig
 brew install autojump
+brew install bash
 brew install direnv
+brew install fzf
+brew install nvm
+brew install pyenv
+brew install pyenv-virtualenv
+brew install redis
 brew install shellcheck
 brew install pass
 brew install tree
+brew install tig
+brew install tmux
+brew install wget
 
 # tmux
 brew install reattach-to-user-namespace
 
-
-echo '[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh' >> ~/.bash_aliases
-
 brew tap neovim/neovim
-brew install --HEAD neovim
+brew install --HEAD --with-release neovim
