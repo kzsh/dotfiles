748ace370178f466bc03c8933da8bf27dcb7a8f4
Andrew Hunt <andrew@hunt.li>
2016-12-04 14:43:29 -0500

    Add hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,12 @@
+-- reload with R
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
+end)
+hs.alert.show("Config loaded")
+
+-- scripts
+dofile("lib/window-positioning.lua")
+
+-- Vendor additions
+dofile("vendor/anycomplete/anycomplete.lua")
+dofile("vendor/anycomplete/anycomplete.lua")
