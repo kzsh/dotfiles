6fdedf66f8e599650c5420eebf115e4b46518db3
Andrew Hunt <andrew@hunt.li>
2016-12-22 10:54:08 -0500

    Speed up terminal startup time with lazy loading nvm

---
+++ install/lazy_sources/nvm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Enable NVM
+echo "$NVM_DIR"
+if [[ -z "$NVM_DIR" ]]; then
+  echo loading nvm
+  # unalias first since nvm.sh just aliases nvm
+  unalias nvm
+  export NVM_DIR=~/.nvm
+  source $(brew --prefix nvm)/nvm.sh
+fi
+
+echo "running nvm"
+nvm "$@"
