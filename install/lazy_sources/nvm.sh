bcfcb5b4737ab35f530b59f9be9b546c320c9841
Andrew Hunt <andrew@hunt.li>
2017-01-10 21:00:24 -0500

    Remove test echos

--- install/lazy_sources/nvm.sh
+++ install/lazy_sources/nvm.sh
@@ -3,12 +3,10 @@
 # Enable NVM
 echo "$NVM_DIR"
 if [[ -z "$NVM_DIR" ]]; then
-  echo loading nvm
+  echo "loading nvm"
   # unalias first since nvm.sh just aliases nvm
   unalias nvm
   export NVM_DIR=~/.nvm
   source $(brew --prefix nvm)/nvm.sh
 fi
-
-echo "running nvm"
 nvm "$@"
