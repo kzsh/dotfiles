726b21199d17d8da42430df5e13c84ee8a47478b
Andrew Hunt <andrew@hunt.li>
2017-02-20 13:27:41 -0500

    Update util to follow google func and var name styles

--- bash/util.sh
+++ bash/util.sh
@@ -7,55 +7,49 @@ COLOR_GREEN=$ESC_SEQ"32;01m"
 COLOR_YELLOW=$ESC_SEQ"33;01m"
 COLOR_CYAN=$ESC_SEQ"36;01m"
 
-function echoSuccess() {
-  echo -e "${COLOR_GREEN}${1}${COLOR_RESET}"
-}
 
-function echoInfo() {
-  echo -e "${COLOR_CYAN}${1}${COLOR_RESET}"
-}
-
-function echoDebug() {
+function echo_error() { >&2 echo -e "${COLOR_RED}${1}${COLOR_RESET}"; }
+function echo_info() { echo -e "${COLOR_CYAN}${1}${COLOR_RESET}"; }
+function echo_success() { echo -e "${COLOR_GREEN}${1}${COLOR_RESET}"; }
+function echo_warn() { echo -e "${COLOR_YELLOW}${1}${COLOR_RESET}"; }
+function echo_debug() {
   if [ -n "$IS_DEBUGGING" ]; then
     echo -e "[DEBUG]${1}"
   fi
 }
 
-function echoWarn() {
-  echo -e "${COLOR_YELLOW}${1}${COLOR_RESET}"
-}
-
-function echoError() {
-  >&2 echo -e "${COLOR_RED}${1}${COLOR_RESET}"
-}
+function log_debug() { echo_debug "$@"; }
+function log_error() { echo_error "$@"; }
+function log_info() { echo_info "$@"; }
+function log_success() { echo_success "$@"; }
+function log_warn() { echo_warn "$@"; }
 
 function goToPath() {
-  cd "$1" || exitError "could not reach directory $1"
+  cd "$1" || exit_error "could not change to directory $1"
 }
 
 
-function beginSection() {
+function begin_section() {
  echoInfo "================================================================================"
  echoInfo " $1"
  echoInfo "================================================================================"
 }
 
-function endSection() {
- echoInfo ""
+function end_section() {
+ echo_info ""
 }
 
 
-function exitSuccess() {
-  echoSuccess "$1"
+function exit_success() {
+  echo_success "$1"
   exit
 }
 
-function exitError() {
-  echoError "${COLOR_RED}${1}${COLOR_RESET}"
+function exit_error() {
+  echo_error "${COLOR_RED}${1}${COLOR_RESET}"
   if [ -n "$2" ]; then
     exit "$2"
   else
     exit 1
   fi
 }
-
