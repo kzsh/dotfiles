f27cbdbe84cd63eb9f94c1160a3bbe5184eee933
Andrew Hunt <andrew@hunt.li>
2017-05-20 15:26:57 -0400

    Add pyenv to path before testing if it exists..

--- install/config_bash.sh
+++ install/config_bash.sh
@@ -5,10 +5,10 @@
 export VIM_DIR="$HOME/.config/nvim"
 
 
+export PYENV_ROOT=$HOME/.pyenv
+export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv >/dev/null 2>&1; then
   eval "$(pyenv init -)"
-  export PYENV_ROOT=$HOME/.pyenv
-  export PATH="$PYENV_ROOT/bin:$PATH"
   debug_log "Loaded: pyenv"
 
   if command -v pyenv >/dev/null 2>&1; then
