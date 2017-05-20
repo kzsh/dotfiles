ce24f50e05fb856bb09925cc6068a0135773831e
Andrew Hunt <andrew@hunt.li>
2017-05-20 15:27:18 -0400

    Use brew only if brew exists

--- install/lazy_sources/nvm.sh
+++ install/lazy_sources/nvm.sh
@@ -7,6 +7,14 @@ if [[ -z "$NVM_DIR" ]]; then
   # unalias first since nvm.sh just aliases nvm
   unalias nvm
   export NVM_DIR=~/.nvm
-  source $(brew --prefix nvm)/nvm.sh
+
+  if command -v brew >/dev/null 2>&1; then
+    . $(brew --prefix nvm)/nvm.sh
+  else
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+  fi
+
 fi
 nvm "$@"
