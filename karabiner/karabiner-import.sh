d9eed0425343dfba403a4d91681170d038b23285
Andrew Hunt <andrew@hunt.li>
2017-06-20 14:42:41 -0400

    Update Karabiner defaults

--- karabiner/karabiner-import.sh
+++ karabiner/karabiner-import.sh
@@ -4,9 +4,29 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set option.vimode_fn_hjkl 1
 /bin/echo -n .
+$cli set parameter.acceleration_of_pointer 10
+/bin/echo -n .
+$cli set parameter.acceleration_of_scroll 100
+/bin/echo -n .
+$cli set parameter.maximum_speed_of_pointer 100
+/bin/echo -n .
+$cli set parameter.maximum_speed_of_scroll 100
+/bin/echo -n .
+$cli set parameter.mousekey_high_speed_of_pointer 100
+/bin/echo -n .
+$cli set parameter.mousekey_high_speed_of_scroll 100
+/bin/echo -n .
+$cli set private.map_right_Command___h_l_to_control_tab___control_shift_tab 1
+/bin/echo -n .
+$cli set private.map_right_command___j_k_to_control_tilde___control_shift_tilde 1
+/bin/echo -n .
+$cli set private.map_right-hand_h_l_to_control_tab___control_shift_tab 1
+/bin/echo -n .
+$cli set private.remap_right_command_semi_quote_to_command_tabs 1
+/bin/echo -n .
 $cli set remap.cmdcmd_to_homerow_numbers 1
 /bin/echo -n .
-$cli set repeat.initial_wait 250
+$cli set repeat.initial_wait 180
 /bin/echo -n .
 $cli set repeat.wait 33
 /bin/echo -n .
