3c7f96c5c2ef752e3c142277060185345f02f829
Andrew Hunt <andrew@hunt.li>
2017-07-18 10:19:08 -0400

    Load completions in one place

---
+++ install/completions.sh
@@ -0,0 +1,15 @@
+completion_sources=(
+  "$HOME/src/scripts/bash/git-completion.sh"
+  "$HOME/src/scripts/bash/hub.bash_completion.sh"
+  "$(brew --prefix pass)/etc/bash_completion.d/pass"
+  "$(brew --prefix nvm)/etc/bash_completion.d/nvm"
+)
+
+for src in "${completion_sources[@]}"; do
+  # -s == if file exists and has size > 0
+  if [[ -s $src ]]; then
+    # shellcheck disable=1090
+    . "$src"
+    debug_log "Loaded: $src"
+  fi
+done
