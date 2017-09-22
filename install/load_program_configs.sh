ea3b02a8b8e32738b34a4951da797216142324aa
Andrew Hunt <andrew@hunt.li>
2017-09-22 12:22:13 -0400

    Add missing pyenv config

--- install/load_program_configs.sh
+++ install/load_program_configs.sh
@@ -1,5 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck disable=1090
+. "$DIR/program_config/pyenv.sh"
+
+# shellcheck disable=1090
 . "$HOME/src/scripts/install/bash_debug_functions.sh"
 
 # shellcheck disable=1090
