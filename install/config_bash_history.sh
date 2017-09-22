a974387ca0841e28701d853c82dffd8d6b51fab1
Andrew Hunt <andrew@hunt.li>
2017-09-22 10:56:09 -0400

    Try to fix bash history not appending correctly

--- install/config_bash_history.sh
+++ install/config_bash_history.sh
@@ -11,4 +11,4 @@ mkdir -p "$HOME/.logs"
 export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi'
 
 # Save and reload the history after each command finishes
-PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
+PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
