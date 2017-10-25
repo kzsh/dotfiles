1e2f31007cb88522ef2039c203ca6756f2890655
Andrew Hunt <andrew@hunt.li>
2017-10-25 13:19:54 -0400

    Add bash function to search given dir and open in vim

--- install/bash_functions.sh
+++ install/bash_functions.sh
@@ -8,3 +8,13 @@ function f () {
   find . -iname "$1"
   #mdfind "*$1*" -onlyin -name .
 }
+function vis() {
+  local dir
+  if [[ -z "$1" ]]; then
+    dir="./"
+  else
+    dir="$1"
+  fi
+
+  nvim $(rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" "$dir" 2> /dev/null  | fzf)
+}
