6c1d9c608e4705434667381c434a84b722965ad5
Andrew Hunt <andrew@hunt.li>
2017-10-25 13:20:18 -0400

    Replace ag with rg

--- install/program_config/fzf.sh
+++ install/program_config/fzf.sh
@@ -1,4 +1,4 @@
 [ -f ~/.fzf.bash ] && . ~/.fzf.bash
-export FZF_DEFAULT_COMMAND='ag -g "" | cut -d " " -f 2- | sort | uniq'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 debug_log "Lazy loaded: fzf"
