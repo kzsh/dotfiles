b05bd334d5d9ca6bec00d71bc9dcfc4b1b23d4fe
Andrew Hunt <andrew@hunt.li>
2017-11-17 17:54:28 -0500

    Remove test code

--- bash/set_space_brightness_from_current_brightness.sh
+++ bash/set_space_brightness_from_current_brightness.sh
@@ -5,5 +5,4 @@ sleep 0.5
 CURRENT_SPACE=$(ls ~/.config/chunkwm/current_space/)
 
 "$HOME/src/scripts/bash/brightness_for_space.sh" "$CURRENT_SPACE" "$CURRENT_BRIGHTNESS"
-echo "$CURRENT_BRIGHTNESS" > ~/tmpfile
 
