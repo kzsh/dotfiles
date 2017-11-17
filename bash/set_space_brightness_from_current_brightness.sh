443fca228156d94343aa37c3c1f7701d239bc94a
Andrew Hunt <andrew@hunt.li>
2017-11-17 15:28:18 -0500

    Fix command references

--- bash/set_space_brightness_from_current_brightness.sh
+++ bash/set_space_brightness_from_current_brightness.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-CURRENT_BRIGHTNESS=$(brightness -l | tail -1 | cut -d ' ' -f4)
+CURRENT_BRIGHTNESS=$(/usr/local/bin/brightness -l | tail -1 | cut -d ' ' -f4)
 CURRENT_SPACE=$(ls ~/.config/chunkwm/current_space/)
 
 "$HOME/src/scripts/bash/brightness_for_space.sh" "$CURRENT_SPACE" "$CURRENT_BRIGHTNESS"
+echo "$CURRENT_BRIGHTNESS" > ~/tmpfile
+
