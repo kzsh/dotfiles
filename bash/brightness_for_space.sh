cf22f20f1ac3248cbc21d5d32f09079fed97e842
Andrew Hunt <andrew@hunt.li>
2017-11-17 18:00:58 -0500

    Add intializer for the file if it doesn't exist

--- bash/brightness_for_space.sh
+++ bash/brightness_for_space.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+BRIGHTNESS_PATH="$HOME/.config/chunkwm/space_brightness"
+BRIGHTNESS_FILE="space_brightness"
 function main() {
+  if [[ -s $BRIGHTNESS_PATH/$BRIGHTNESS_FILE ]]; then
+    mkdir -p $BRIGHTNESS_PATH
+    cat << EOF > "$BRIGHTNESS_FILE"
+q=0.5
+w=0.5
+e=0.5
+r=0.5
+t=0.5
+a=0.5
+s=0.5
+d=0.5
+f=0.5
+g=0.5
+z=0.5
+x=0.5
+c=0.5
+v=0.5
+b=0.5
+EOF
+  fi
+
   if [[ -n "$2" ]]; then
     brightness_setting=$(set_setting_for_space "$1" "$2")
   else
