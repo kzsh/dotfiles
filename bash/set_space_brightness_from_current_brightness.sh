6b795e621a43ae322aa9c9e9518f434a991c99b3
Andrew Hunt <andrew@hunt.li>
2017-11-19 15:07:54 -0500

    Remove extraneous newline

--- bash/set_space_brightness_from_current_brightness.sh
+++ bash/set_space_brightness_from_current_brightness.sh
@@ -5,4 +5,3 @@ CURRENT_BRIGHTNESS=$(/usr/local/bin/brightness -l | tail -1 | cut -d ' ' -f4)
 CURRENT_SPACE=$(ls ~/.config/chunkwm/current_space/)
 
 "$HOME/src/scripts/bash/brightness_for_space.sh" "$CURRENT_SPACE" "$CURRENT_BRIGHTNESS"
-
