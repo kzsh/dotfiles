9f3cb26936e735a26d539c07c45a4e77fe87923b
Andrew Hunt <andrew@hunt.li>
2017-12-08 16:10:02 -0500

    Add shell script to send email

---
+++ bash/send_email.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+function main() {
+  if [[ $# -eq 0 ]] ; then
+    usage
+    exit 0
+  fi
+
+  if [[ -z "$1" ]]; then echo "  Error: Must specify a recipient."
+    usage
+  fi
+
+  if [[ -z "$2" ]]; then
+    echo "  Error: Must specify a subject."
+    usage
+  fi
+
+  HEADER="To: $1\nSubject: $2\nContent-Type: text/plain"
+
+  BODY="${*:3}"
+  echo -e "${HEADER}\n\n${BODY}" | sendmail -t
+}
+
+function usage() {
+  cat <<-EOS
+
+  Usage: send_email <recipient> <subject> [body]
+
+  recipient	A valid email address
+  subject	A valid email subject line
+  body		A valid email body (optional)
+EOS
+}
+
+main "$@"
