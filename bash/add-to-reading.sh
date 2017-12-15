dc6a55003bf4141b949c10c1c373a8b8ce61f86c
Andrew Hunt <andrew@hunt.li>
2017-12-15 16:38:26 -0500

    Follow symlink for script dir

--- bash/add-to-reading.sh
+++ bash/add-to-reading.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [[ -L $0 ]]; then
+  SCRIPT_DIR="$(dirname "$(greadlink -f "$0")")"
+else
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+
 BODY="${*}"
 
 "$SCRIPT_DIR/send_email.sh" "andrew@hunt.li" "[automated] reading" "$BODY"
