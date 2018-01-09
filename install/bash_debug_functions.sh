5fb735fe6b91f7b7916a26e798cbac4092a4401c
Andrew Hunt <andrew@hunt.li>
2018-01-09 14:20:31 -0500

    Fix debug function condition

--- install/bash_debug_functions.sh
+++ install/bash_debug_functions.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-if [[ -z "$LOADED_DEBUG_FUNCTIONS" ]]; then
+if [[ -z "$__LOADED_DEBUG_FUNCTIONS" ]]; then
   function get_time() {
-    echo $(python -c 'import time; print time.time()')
+    echo "$(python -c 'import time; print time.time()')"
   }
 
   LAST_TIME=$(get_time)
 
   function debug_log() {
-
-    if [ -z ${DEBUG_STARTUP+x} ]; then
+    if [ -n "${DEBUG_STARTUP}" ]; then
       CURRENT_TIME=$(get_time)
       DIFF=$(echo "$CURRENT_TIME - $LAST_TIME" | bc)
       echo -n "$@"
@@ -17,6 +16,6 @@ if [[ -z "$LOADED_DEBUG_FUNCTIONS" ]]; then
       LAST_TIME=$CURRENT_TIME
     fi
   }
-  LOADED_DEBUG_FUNCTIONS=1
-fi
 
+  export __LOADED_DEBUG_FUNCTIONS=1
+fi
