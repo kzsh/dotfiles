60e8f4f4bceeb0c5c6073ba3ab7d28632f6d88eb
Andrew Hunt <andrew@hunt.li>
2018-01-10 10:08:35 -0500

    Add method for generating lazy-loaded aliases for programs

--- install/load_program_configs.sh
+++ install/load_program_configs.sh
@@ -1,4 +1,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function build_aliases() {
+  local target_function
+  target_function=$1
+  aliased_functions=${*:2}
+
+  for target in "${aliased_functions[@]}"; do
+    # -s == if file exists and has size > 0
+    debug_log "Configuring lazy load for: $target_function ($target)"
+    alias $target="$target_function $target"
+  done
+}
+
 # shellcheck disable=1090
 . "$DIR/program_config/pyenv.sh"
 
