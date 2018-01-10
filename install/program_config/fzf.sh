fb88e8bc868d23daead8411932c21f4148462b94
Andrew Hunt <andrew@hunt.li>
2018-01-10 10:10:07 -0500

    Convert fzf to lazy-loading alias

--- install/program_config/fzf.sh
+++ install/program_config/fzf.sh
@@ -1,4 +1,13 @@
-[ -f ~/.fzf.bash ] && . ~/.fzf.bash
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-debug_log "Lazy loaded: fzf"
+#!/bin/bash
+
+function lazy_load_fzf() {
+  debug_log "Lazy loading: fzf"
+  unalias fzf
+  [ -f ~/.fzf.bash ] && . ~/.fzf.bash
+  export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+  fzf "$@"
+}
+
+alias fzf='lazy_load_fzf'
