d9220d111e67e7546b4494c0ba9298a8d5c576ea
Andrew Hunt <andrew@hunt.li>
2018-02-10 13:30:13 -0500

    Add semi-colons

--- karabiner-elements/chunkwm-support.js
+++ karabiner-elements/chunkwm-support.js
@@ -4,14 +4,14 @@ const path = require('path');
 const fs = require('fs');
 const writeFile = promisify(fs.writeFile);
 
-const buildWorkspaceChangeCommands = require('./chunkwm-support-workspaces')
-const buildBrightnessCommands = require('./chunkwm-support-brightness')
+const buildWorkspaceChangeCommands = require('./chunkwm-support-workspaces');
+const buildBrightnessCommands = require('./chunkwm-support-brightness');
 
-const OUT_DIR = path.resolve(__dirname, "assets")
-const OUT_FILE = path.resolve(OUT_DIR, "chunkwm-support.json")
+const OUT_DIR = path.resolve(__dirname, "assets", "complex_modifications");
+const OUT_FILE = path.resolve(OUT_DIR, "chunkwm-support.json");
 
 function writeResult(data) {
-  writeFile(OUT_FILE, JSON.stringify(data, null, 2))
+  writeFile(OUT_FILE, JSON.stringify(data, null, 2));
 }
 
 function buildFile() {
