5a56d5e8e53667082e4a56e9b07a48aa7ad38ec2
Andrew Hunt <andrew@hunt.li>
2018-02-12 14:39:20 -0500

    Defined functions without less-supported keyword

--- bash/util.sh
+++ bash/util.sh
@@ -8,44 +8,43 @@ COLOR_YELLOW=$ESC_SEQ"33;01m"
 COLOR_CYAN=$ESC_SEQ"36;01m"
 
 
-function echo_error() { >&2 echo -e "${COLOR_RED}${1}${COLOR_RESET}"; }
-function echo_info() { echo -e "${COLOR_CYAN}${1}${COLOR_RESET}"; }
-function echo_success() { echo -e "${COLOR_GREEN}${1}${COLOR_RESET}"; }
-function echo_warn() { echo -e "${COLOR_YELLOW}${1}${COLOR_RESET}"; }
-function echo_debug() {
+echo_error() { >&2 echo -e "${COLOR_RED}${1}${COLOR_RESET}"; }
+echo_info() { echo -e "${COLOR_CYAN}${1}${COLOR_RESET}"; }
+echo_success() { echo -e "${COLOR_GREEN}${1}${COLOR_RESET}"; }
+echo_warn() { echo -e "${COLOR_YELLOW}${1}${COLOR_RESET}"; }
+echo_debug() {
   if [ -n "$IS_DEBUGGING" ]; then
     echo -e "[DEBUG]${1}"
   fi
 }
 
-function log_debug() { echo_debug "$@"; }
-function log_error() { echo_error "$@"; }
-function log_info() { echo_info "$@"; }
-function log_success() { echo_success "$@"; }
-function log_warn() { echo_warn "$@"; }
+log_debug() { echo_debug "$@"; }
+log_error() { echo_error "$@"; }
+log_info() { echo_info "$@"; }
+log_success() { echo_success "$@"; }
+log_warn() { echo_warn "$@"; }
 
-function goToPath() {
+goToPath() {
   cd "$1" || exit_error "could not change to directory $1"
 }
 
-
-function begin_section() {
+begin_section() {
  echoInfo "================================================================================"
  echoInfo " $1"
  echoInfo "================================================================================"
 }
 
-function end_section() {
+end_section() {
  echo_info ""
 }
 
 
-function exit_success() {
+exit_success() {
   echo_success "$1"
   exit
 }
 
-function exit_error() {
+exit_error() {
   echo_error "${COLOR_RED}${1}${COLOR_RESET}"
   if [ -n "$2" ]; then
     exit "$2"
