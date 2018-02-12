21b7f565ef8360c9c8c57923b37a4e58070f641d
Andrew Hunt <andrew@hunt.li>
2018-02-12 14:37:29 -0500

    Update control flow utils

--- bash/utils/control_flow.sh
+++ bash/utils/control_flow.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-function exit_with_message() {
+exit_success() {
+  exit_with_message "$1" "0"
+}
+
+exit_with_message() {
   local message
   local exit_code
   message=$1
+
+  # exit with given code or 1
   exit_code="${2:-1}"
   echo "$message ($exit_code)"
   exit "$exit_code"
