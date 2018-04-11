0cb5678b80e15e613ba753ec7146b05a661e3d7a
Andrew Hunt <andrew@hunt.li>
2018-04-11 16:03:22 -0400

    Remove indirection

--- install/program_config/nvm.sh
+++ install/program_config/nvm.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-LAZY_COMMAND="nvm"
 function lazy_load_nvm() {
   local original_command
   local args
   original_command="$1"
   args=${*:2}
-  debug_log "Lazy loading: $LAZY_COMMAND"
-  unalias "$LAZY_COMMAND"
+  debug_log "Lazy loading: nvm"
+  unalias "nvm"
 
   NVM_DIR=~/.nvm
   export NVM_DIR
-  source $(brew --prefix nvm)/nvm.sh
+  . /usr/local/opt/nvm/nvm.sh
+
 
   # shellcheck disable=SC1090
-  . "$(brew --prefix $LAZY_COMMAND)/etc/bash_completion.d/$LAZY_COMMAND"
+  . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
 
   # shellcheck disable=SC2086
   "$original_command" $args
 }
 
-build_aliases "lazy_load_nvm" "$LAZY_COMMAND"
+build_aliases "lazy_load_nvm" "nvm"
