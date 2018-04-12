19291cd8b57ea17b6909c6e8c328e8912a7d2f60
Andrew Hunt <andrew@hunt.li>
2018-04-11 21:07:12 -0400

    Remove debug logging from pyenv

--- install/program_config/pyenv.sh
+++ install/program_config/pyenv.sh
@@ -1,7 +1,5 @@
 export PYENV_ROOT=$HOME/.pyenv
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-debug_log "Loaded: pyenv"
 eval "$(pyenv virtualenv-init -)"
-debug_log "Loaded: virtual-env"
 
