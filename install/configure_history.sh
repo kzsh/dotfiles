fbc58245c67c1f55464173ac3fbab66e57bb2169
Andrew Hunt <andrew@hunt.li>
2018-04-26 08:17:26 -0400

    Clean up history configuration file

--- install/configure_history.sh
+++ install/configure_history.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 export HISTCONTROL=ignoredups:erasedups
-export HISTSIZE=1000000                   # big big history
+export HISTSIZE=100
 export HISTFILESIZE=1000000               # big big history
 
 shopt -s histappend                      # append to history, don't overwrite it
 shopt -s histverify
 
-mkdir -p "$HOME/.logs"
+if [[ ! -f $HOME/.logs ]]; then
+  mkdir -p "$HOME/.logs"
+fi
 
 export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi'
-
-# Save and reload the history after each command finishes
-PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 # fh - search command history in ~/.logs (all history)
 fh() {
