38e07867486e61d39f2e17535734962a2ba1e856
Andrew Hunt <andrew@hunt.li>
2018-05-10 17:57:58 -0400

    Remove languishing install scripts

--- install/install_homebrew.sh
+++
@@ -1,7 +0,0 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Allow fixing to specific versions
-brew tap homebrew/boneyard
-brew tap caskroom/versions
-
-brew install editorconfig
