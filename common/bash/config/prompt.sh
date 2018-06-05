a1430a9bfdfac5c1e2cf45c4e2bf80a308c740eb
Andrew Hunt <andrew@hunt.li>
2018-06-05 00:07:57 -0400

    Restructure prompt

--- common/bash/config/prompt.sh
+++ common/bash/config/prompt.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Solarized colors
-# https://github.com/altercation/solarized/tree/master/iterm2-colors-solarized
+# Colors
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
-SOLAR_YELLOW=$(tput setaf 136)
-PURPLE=$(tput setaf 260)
-SOLAR_ORANGE=$(tput setaf 166)
-SOLAR_BLUE=$(tput setaf 33)
-SOLAR_CYAN=$(tput setaf 37)
-SOLAR_GREEN=$(tput setaf 64)
-SOLAR_WHITE=$(tput setaf 254)
+RED=$(tput setaf 1)
+SOLAR_YELLOW=$(tput setaf 3)
+PURPLE=$(tput setaf 13)
+SOLAR_ORANGE=$(tput setaf 9)
+SOLAR_BLUE=$(tput setaf 4)
+SOLAR_CYAN=$(tput setaf 6)
+SOLAR_GREEN=$(tput setaf 2)
+SOLAR_WHITE=$(tput setaf 7)
 
 # Prompt styles
 style_user="\[${RESET}${SOLAR_YELLOW}\]"
@@ -22,19 +22,46 @@ style_branch="${SOLAR_YELLOW}"
 style_virtualenv="${BOLD}${SOLAR_ORANGE}"
 style_has_jobs="${SOLAR_YELLOW}"
 style_job_count="${PURPLE}"
+style_last_exit_code_bad="${RED}"
 
 style_git_unstaged="${RESET}${SOLAR_GREEN}"
 style_git_staged="${RESET}${SOLAR_YELLOW}"
 style_git_untracked="${RESET}${SOLAR_CYAN}"
 
+# Misc config
+
 GIT_DIFF_CHAR="•"
-# Show the commit status of the current git repo
-function git_repo_state() {
-  local status
-  status="$(git status 2>/dev/null | tail -n1)"
-  [[ $status != *"nothing to commit"* ]] && echo " [!]";
+
+# Main methods
+__build_prompt() {
+  build_ps1
+  export PROMPT_COMMAND="export_last_exit_code; log_history; $PROMPT_COMMAND"
 }
 
+build_ps1() {
+  # Build the prompt
+  PS1="\033]0;\a"
+  PS1+="\n"
+  if [[ "$SSH_TTY" ]]; then
+    PS1+="${style_important}[SSH] " # [SSH]
+  fi
+  PS1+="\$(has_jobs)"
+  PS1+="${style_chars}:: ${style_path}\w" # : directory
+  if [[ -z "$SSH_TTY" ]]; then
+    PS1+="\$(prompt_git)" # Git details
+    PS1+="\$(prompt_virtualenv)" # Virtualenv details
+  fi
+  PS1+="\n"
+  PS1+="${style_chars}\$ \[${RESET}\]"
+}
+
+__initialization() {
+  if [[ ! -f $HOME/.logs ]]; then
+    mkdir -p "$HOME/.logs"
+  fi
+}
+
+# Local methods
 function has_jobs() {
   job_count=$(jobs -l | awk '{ print $3 }' | grep -vc "Done")
   prompt_jobs="[${style_job_count}$job_count${style_has_jobs}] "
@@ -62,7 +89,7 @@ function prompt_git() {
   if [[ "$flags" ]]; then
     output="$output[${flags}${style_branch}]"
   fi
-  echo -ne "${RESET}${SOLAR_BLUE} on ${style_branch}${output}" # $(git_repo_state)"
+  echo -ne "${RESET}${SOLAR_BLUE} on ${style_branch}${output}"
 }
 
 # Show the name of the current virtualenv
@@ -75,48 +102,20 @@ function prompt_virtualenv() {
   fi
 }
 
-function tmux_rename_window() {
-  case "$TERM" in
-    screen*)
-      tmux rename-window "$(last_command)"
-      ;;
-  esac
-}
-
-function set_iterm2_profile() {
-  is_inverted=$(cat ~/.config/chunkwm/inversion_enabled)
-  if [[ -z "$is_inverted" ]]; then
-    if [[ "$is_inverted" == "1" ]]; then
-      theme="Default Bright"
-    else
-      theme="Default Dark"
-    fi
-
-    if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
-      echo -e "\033]50;SetProfile=$theme\a"
-    else
-      echo ""
-    fi
+log_history() {
+  if [[ "$(id -u)" -ne 0 ]]; then
+    data="$(date "+%Y-%m-%d.%H:%M:%S")	$(pwd)	$(last_exit_code)	$(history 1)"
+    echo "$data" >> ~/.logs/bash-history-"$(date "+%Y-%m-%d")".log
   fi
 }
 
-# Build the prompt
-PS1="\033]0;\a"
-# Reset to insert mode between prompts
-PS1+="\ei"
+export_last_exit_code() {
+  export __KZSH__LAST_EXIT_CODE="$?"
+}
 
-# PS1+="\$(tmux_rename_window)"
-# PS1+="\$(set_iterm2_profile)"
-PS1+="\n"
-if [[ "$SSH_TTY" ]]; then
-  PS1+="${style_important}[SSH] " # [SSH]
-fi
-PS1+="\$(has_jobs)"
-#PS1+="${style_user}\u${style_chars}" # @${style_host}\h" # username@host
-PS1+="${style_chars}:: ${style_path}\w" # : directory
-if [[ -z "$SSH_TTY" ]]; then
-  PS1+="\$(prompt_git)" # Git details
-  PS1+="\$(prompt_virtualenv)" # Virtualenv details
-fi
-PS1+="\n"
-PS1+="${style_chars}\$ \[${RESET}\]"
+last_exit_code() {
+  echo "$__KZSH__LAST_EXIT_CODE"
+}
+
+__initialization
+__build_prompt
