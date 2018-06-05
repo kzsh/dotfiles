7170a545164183e3ee30d4f7a67979081f677f64
Andrew Hunt <andrew@hunt.li>
2018-06-05 00:09:22 -0400

    Simplify history command

--- common/bash/config/configure_history.sh
+++ common/bash/config/configure_history.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-export HISTCONTROL=erasedups
-export HISTSIZE=100
-export HISTFILESIZE=1000000
 
-shopt -s histappend
+export HISTCONTROL=ignoredups,ignorespace
+export HISTSIZE=100
+export HISTFILESIZE=1000000               # big big history
+
+shopt -s histappend                      # append to history, don't overwrite it
 shopt -s histverify
 
-if [[ ! -f $HOME/.logs ]]; then
-  mkdir -p "$HOME/.logs"
-fi
-
-export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi'
-
-# Save and reload the history after each command finishes
-PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+export PROMPT_COMMAND="history -a; history -c; history -r;$PROMPT_COMMAND"
