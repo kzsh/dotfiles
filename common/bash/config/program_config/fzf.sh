8a450a517ad60ea822f272347852746ce788e95a
Andrew Hunt <andrew@hunt.li>
2019-04-11 20:23:44 -0400

    Partially complete fzf completion

--- common/bash/config/program_config/fzf.sh
+++ common/bash/config/program_config/fzf.sh
@@ -4,4 +4,10 @@ export FZF_DEFAULT_OPTS='--bind=ctrl-alt-j:down,ctrl-alt-k:up,ctrl-d:page-down,c
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+export FZF_COMPLETION_TRIGGER='~~'
+# bindkey '^T' fzf-completion
+# bindkey '^I' fzf-completion
+# bind -x '"\C-i": "fzf-completion"'
+# bind -x '"\C-i": "expand-or-complete"'
+
 [ -f ~/.fzf.bash ] && . ~/.fzf.bash
