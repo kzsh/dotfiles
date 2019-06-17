feaf59838f32f8735513f52a78c43e184f9b6ade
Andrew Hunt <andrew@hunt.li>
2019-06-16 20:06:27 -0400

    Fix (-ish) util calling script dir script

--- bash/utils/script_dir.sh
+++ bash/utils/script_dir.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-echo "$( cd "$( dirname "${BASH_SOURCE[-1]}" )" && pwd )"
+#!/usr/bin/env bash
+
+# USAGE: call from sourced scripts (1 layer deep, it's fragile)
+# to get the sourcer's directory
+
+EXEC_DIR="$( cd "$( dirname "${BASH_SOURCE[-2]}" )" && pwd )"
