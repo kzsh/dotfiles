62c6d34df9aefaf3a155a096dae069b35070da8d
Andrew Hunt <andrew@hunt.li>
2019-11-21 16:49:32 -0500

    More updates

---
+++ macos/bitbar/datetime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# <bitbar.title>Display a compact date and time</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Andrew Hunt</bitbar.author>
+# <bitbar.author.github>kzsh</bitbar.author.github>
+# <bitbar.desc>Display a compact date and time</bitbar.desc>
+#
+
+echo "$(date +%m-%d\ %H:%M)"
