c2d025dd34a1ead2cc591138ead84f83f216b7f3
Andrew Hunt <andrew@hunt.li>
2019-11-27 08:52:53 -0500

    Move mac-specific scripts to mac section

--- scripts/bash/macOS/notify-do.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-"$@" && notify "$@ succeeded" || notify "$@ failed"
--- scripts/bash/macOS/reset_coreaudiod.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo launchctl unload /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist && sudo launchctl load /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist && notify "coreaudiod has been reset"
