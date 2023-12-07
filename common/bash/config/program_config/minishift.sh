6a5a24435fc9d86aec481588dc4effd887ba8807
Andrew Hunt <andrew@hunt.li>
2023-12-07 11:26:59 -0500

    Replace explicit user paths where possible

--- common/bash/config/program_config/minishift.sh
+++ common/bash/config/program_config/minishift.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env/bash
-export PATH="/Users/andrewhunt/.minishift/cache/oc/v3.11.0/darwin:$PATH"
+export PATH="$HOME/.minishift/cache/oc/v3.11.0/darwin:$PATH"
 # Run this command to configure your shell:
 if minishift status | grep -q Running; then
   eval "$(minishift oc-env)"
