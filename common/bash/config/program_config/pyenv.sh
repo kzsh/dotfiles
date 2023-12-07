7e014e10b8010603b2ab9df4d7f007c7b0239874
Andrew Hunt <andrew@hunt.li>
2023-12-07 10:57:59 -0500

    Source pyenv directly to avoid inconsistency

--- common/bash/config/program_config/pyenv.sh
+++ common/bash/config/program_config/pyenv.sh
@@ -2,10 +2,11 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export PYENV_ROOT=$HOME/.pyenv
 export PATH="$PYENV_ROOT/bin:$PATH"
 
-function init-pyenv-fn() {
-  export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-}
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+# function init-pyenv-fn() {
+#   eval "$(pyenv init -)"
+#   eval "$(pyenv virtualenv-init -)"
+# }
 
-alias init-pyenv="init-pyenv-fn"
+# alias init-pyenv="init-pyenv-fn"
