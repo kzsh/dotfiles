79b62273c78cd5f811e0ee3c8f06745530452b92
kzsh <kzsh@kzsh.dev>
2025-04-05 12:28:12 -0400

    Add prettier output for git-op

--- common/scripts/bash/git/git-op.sh
+++ common/scripts/bash/git/git-op.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
-PWD=$(pwd)
+
+CURRENT_DIR="$(pwd)"
+
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
+# Utility Functions
+banner() {
+  columns="$(tput cols)"
+  printf "\n"
+  printf "\n"
+  printf "%b" "${BOLD}"
+  printf "%0.s=" $(seq 1 $columns)
+  printf "\n"
+  echo "$*"
+  printf "%0.s=" $(seq 1 $columns)
+  printf "%b" "${NORMAL}"
+}
+
+echo_bold() {
+  echo "${BOLD}$*${NORMAL}"
+}
+
 forEachGitRepo() {
+  COMMAND="$*"
   for r in */; do
     if [ -d "$r/.git" ]; then
-      cd $r || exit 1
-      escaped=$(echo $r | sed 's/\//\\\//g')
-      eval "$1" | sed "s/^/$escaped /g"
-      cd .. || exit 1
+      cd "$r" || exit 1
+      echo
+      banner "$r"
+      eval "${COMMAND}"
+      # escaped=$(echo $r | sed 's/\//\\\//g')
+      # eval "$1" | sed "s/^/$escaped /g"
+      cd "$CURRENT_DIR" || exit 1
     fi
 
   done
