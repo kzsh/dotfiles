51065bb36b1d84df35aade6f6f278da98306d34a
kzsh <kzsh@kzsh.dev>
2025-11-17 22:50:51 -0500

    Remove duplicate env vars from main

--- common/bash/config/main.sh
+++ common/bash/config/main.sh
@@ -13,34 +13,17 @@ export DEBUG_STARTUP=
 
 XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CONFIG_HOME
-
-export DOCKER_SCAN_SUGGEST=false
 export EDITOR=/usr/local/bin/nvim
+export PAGER=less
 
-test_available_vim() {
-  if ! command -v nvim > /dev/null 2>&1; then
-    echo "Neovim not available could not configure editor"
-  fi
-}
-
-test_available_vim &
+if ! command -v nvim > /dev/null 2>&1; then
+  echo "Neovim not available could not configure editor"
+fi
 
 # shopt -s globstar
 # shopt -s extglob
 shopt -s direxpand
 
-# Assume brew prefix to avoid costly `brew --prefix` operation but verify it
-# out-of-band and notify if it's not right.
-
-export BREW_PATH=/usr/local
-test_brew_prefix() {
-  if [[ "$(brew --prefix)" != "$BREW_PATH" ]]; then
-    echo "MISMATCHED BREW PREFIX.  Update \$BREW_PATH from '$BREW_PATH', to '$(brew --prefix)'"
-  fi
-
-}
-
-# test_brew_prefix &
 if [ -f "$BREW_PATH/etc/bash_completion" ]; then
   . "$BREW_PATH/etc/bash_completion"
 fi
@@ -54,16 +37,11 @@ if [ -f /etc/profile.d/bash_completion.sh ]; then
   . /etc/profile.d/bash_completion.sh
 fi
 
-if type brew &>/dev/null; then
-  HOMEBREW_PREFIX="$(brew --prefix)"
-  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
-    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-  else
-    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
-      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
-    done
-  fi
-fi
+# for completion in "$HOME/bin/__completions/"*; do
+#   [[ -r "${completion}" ]] && source "${completion}"
+# done
+
+PATH="$HOME/bin:$PATH"
 
 [[ -n "$DEBUG_STARTUP" ]] && . "$SCRIPT_DIR/debug_functions.sh"
 
@@ -79,7 +57,7 @@ for src in "${sources[@]}"; do
   # -s == if file exists and has size > 0
   if [[ -s $src ]]; then
     #shellcheck disable=1090
-    . "$src"
+    source "$src"
     [[ -n "$DEBUG_STARTUP" ]] && debug_log "Loaded: $src"
   fi
 done
