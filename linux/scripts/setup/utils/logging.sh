280dc4d150e0a765b915efea7b0772e1a8e4b5a1
kzsh <kzsh@kzsh.dev>
2026-08-07 13:39:00 -0400

    Guide against no colors

--- linux/scripts/setup/utils/logging.sh
+++ linux/scripts/setup/utils/logging.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-bold="$(tput bold)"
-normal="$(tput sgr0)"
+if [ -t 1 ] && command -v tput >/dev/null 2>&1; then
+  bold="$(tput bold)"
+  normal="$(tput sgr0)"
+else
+  bold=""
+  normal=""
+fi
 
 vlog() {
   if [[ -n $VERBOSE ]]; then
